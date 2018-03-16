c630792f1f02bb0043eee6989254aa1862d56b95
Luis Miguel Marmolejo Peña <lucncd@gmail.com>
2018-03-16 01:30:22 -0500

    Implementacion Modo 8 bits
    Se  implementan las opciones de control a 8bits con libreria y a Pedal(paso a paso).

--- LCD a Pedal.c
+++ LCD a Pedal.c
@@ -1,16 +1,16 @@
 #include <16F887.h>
 #use delay (crystal=8000000)
 
+//Funcion pulso al pin E de la LCD
 void pulso(){
-   //output_d(0x02);
    output_high(pin_D1);
    delay_ms(500);   
    
-   //output_d(0x00);
    output_low(pin_D1);
    delay_ms(500);
 }
 
+//funcion pulso con escritura incluido
 void escribe(){
    output_d(0x03);
    delay_ms(100);   
@@ -20,13 +20,15 @@ void escribe(){
 }
   
 void main(){
-
+   //Establecemos puertos como salidas
    set_tris_c(0x00);
    set_tris_d(0x00);
+   //Limpiamos los puertos
    output_c(0x00);
    output_d(0x00);
    //--------------------------------------------------------------------------
-   output_c(0x02);
+   //Incializamos la LCD Modo 4 bits
+   output_c(0x20);
    delay_ms(100);
    
    pulso();
@@ -37,37 +39,42 @@ void main(){
    
    pulso();
    //--------------------------------------------------------------------------
-   output_c(0x0F);
+   output_c(0xF0);
    delay_ms(100);
    
    pulso();
    //--------------------------------------------------------------------------
-   //Terrmina inicializacion
+   //Terrmina inicialización
    //Escribe U
-   output_c(0X05);
+   //output_c(0X05);
+   output_c(0X50);
    delay_ms(100);
    
    escribe();
    
-   output_c(0X05);
+   //output_c(0X05);
+   output_c(0X50);
    delay_ms(100);
    
    escribe();
    //--------------------------------------------------------------------------
    //Escribe T
-   output_c(0X05);
+   //output_c(0X05);
+   output_c(0X50);
    delay_ms(100);
    
    escribe();
    
-   output_c(0X04);
+   //output_c(0X04);
+   output_c(0X40);
    delay_ms(100);
    
    escribe();
    
    //--------------------------------------------------------------------------
    //Escribe P
-   output_c(0X05);
+   //output_c(0X05);
+   output_c(0X50);
    delay_ms(100);
    
    escribe();
--- LCD con libreria.c
+++ LCD con libreria.c
@@ -1,18 +1,11 @@
-#include <16f887.h>                   //Nuestro pic
-#use delay (clock=8000000)            //Fosc=8Mhz
-#include <lcd.c>                      //Libreria de la lcd
- 
- 
+#include <16f887.h>                   
+#use delay (clock=8000000)           
+
+#INCLUDE <lcd.c> //Libreria LCD
+
 void main()
 {
-   //setup_oscillator(OSC_8MHZ|OSC_INTRC);  //Programamos oscilador interno del Pic a 8MHZ
-   //set_tris_b(0);                         //Puerto B como salida
+   set_tris_D(0);//Por defecto es el puerto D por lo tanto lo establecemos como salida
    lcd_init();                            //Inicializamos LCD
-   lcd_putc("Hola Mundo");                //"Hola Mundo"
-//!   while(1)
-//!   {
-//!      delay_ms(10);                       //Esperamos a lo sonso 10 ms
-//!      output_toggle(PIN_B5);              //Parpadeamos Led, output_toggle cambia el estado logico del pin pasado de parametro
-//!      delay_ms(250);                     //Esperamos a lo sonso 250 ms
-//!   }
+   lcd_putc("Hola Mundo UTP");                //"Hola Mundo"
 }
